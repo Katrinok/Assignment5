af368938b834f6e7e5afa57b7ca4505d3295f90b
SaevarOrn99 <Saevarorn99@gmail.com>
2023-10-21 13:29:07 +0000

    s

--- server.cpp
+++ server.cpp
@@ -253,13 +253,13 @@ int connectToServer(const std::string& ip_address, int port, std::string groupID
     char responseBufferNY[1025]; // Buffer to hold the response
     memset(responseBufferNY, 0, sizeof(responseBufferNY)); // Clear the buffer
 
-    int bytesRead2 = recv(serverSock, responseBufferNY, sizeof(responseBuffer)-1, 0); // Receive the data
-    if(bytesRead2 < 0) {
+    int bytesRea = recv(serverSock, responseBufferNY, sizeof(responseBuffer)-1, 0); // Receive the data
+    if(bytesRea < 0) {
         perror("Error receiving response from server");
         close(serverSock);
         return -1;
     }
-    else if(bytesRead2 == 0) {
+    else if(bytesRea == 0) {
         std::cout << "Server closed connection after sending QUERYSERVERS" << std::endl;
         close(serverSock);
         return -1;
